84d4789565bc19d792200d7996874b4a7b2e308f
Agent Thick <62157048+AgentKenway@users.noreply.github.com>
2022-07-02 14:28:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import keyboard
+import pyautogui as auto
+import time as wait
+
+
 print("Press escape to start the farm")
 
 while True:
     if keyboard.is_pressed('esc'):
         print("Farming now in progress, terminate this program to stop farming.")
-        import time as wait
 
         wait.sleep(0)
         enabled = True
-        import pyautogui as auto
 
         while enabled:
-            fight = auto.locateCenterOnScreen('fight.png')
-            run = auto.locateCenterOnScreen('run.png')
-            elite = auto.locateCenterOnScreen('ELITE.PNG')
-
+            fight = auto.locateCenterOnScreen('fight.png',region=(640,600,300,150), grayscale=False, confidence=0.95)
+            run = auto.locateCenterOnScreen('run.png',region=(640,600,300,150), grayscale=False, confidence=0.95)
+            elite = auto.locateCenterOnScreen('ELITE.PNG',region=(620,340,100,50), grayscale=False, confidence=0.6)
+            forgetmove = auto.locateCenterOnScreen('forget.png',region=(960,540,100,50), grayscale=False, confidence=0.7)
 
             def clickFight():
                 auto.click(fight)
@@ -23,10 +25,20 @@ while True:
             def clickRun():
                 auto.click(run)
 
+            def clickForget():
+                auto.click(forgetmove)
+            
 
             if elite:
                 print(elite)
                 clickRun()
+                auto.leftClick()
+                
+            elif forgetmove:
+                print(forgetmove)
+                clickForget()
+                auto.leftClick()
+
             elif fight:
                 print(fight)
                 clickFight()
@@ -35,6 +47,8 @@ while True:
                 wait.sleep(0.3)
                 auto.leftClick()
                 auto.moveTo(960, 540)
+  
+            
 
             else:
                 auto.keyDown('A')
